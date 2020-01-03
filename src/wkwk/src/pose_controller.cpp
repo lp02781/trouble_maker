41e5f44d1a6bf1531e01258c36ebe82b076fc949
lp02781 <lp02781@gmail.com>
2020-01-03 21:45:22 +0700

    first

--- src/wkwk/src/pose_controller.cpp
+++ src/wkwk/src/pose_controller.cpp
@@ -1,3 +1,10 @@
+//make reset button
+//make start button
+//make sequential program per QR
+
+//hw/cam_nav
+//hw/cam_dock
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -11,28 +18,43 @@ using namespace cv;
 
 int width = 640;
 int height = 200;
-int x_init_red=0;
-int y_init_red=180;
-int Noise_red 	= 15;
-
+int x_init=0;
+int y_init=180;
+int Noise 	= 15;
 
 void imageProcessing(Mat input_image);
 
-Mat receive_image;
+Mat nav_image;
+Mat dock_image;
 
-void imageCallback(const sensor_msgs::CompressedImageConstPtr& msg)
+void navCallback(const sensor_msgs::CompressedImageConstPtr& msg)
 {
   try
   {
-    receive_image = cv::imdecode(cv::Mat(msg->data),1);//convert compressed image data to cv::Mat
+    nav_image = cv::imdecode(cv::Mat(msg->data),1);//convert compressed image data to cv::Mat
     waitKey(10);
-    imageProcessing(receive_image);
+    imageProcessing(nav_image);
   }
   catch (cv_bridge::Exception& e)
   {
     ROS_ERROR("Could not convert to image!");
   }
 }
+
+/*
+void dockCallback(const sensor_msgs::CompressedImageConstPtr& msg)
+{
+  try
+  {
+    dock_image = cv::imdecode(cv::Mat(msg->data),1);//convert compressed image data to cv::Mat
+    waitKey(10);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert to image!");
+  }
+}
+*/
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "pose");
@@ -41,60 +63,36 @@ int main(int argc, char **argv){
 	
 	image_transport::ImageTransport it(nh);
 	
-	ros::Subscriber sub = nh.subscribe("camera/image/compressed", 1, imageCallback);
+	ros::Subscriber sub_nav 	= nh.subscribe("/hw/cam_nav", 1, navCallback);
+	//ros::Subscriber sub_dock 	= nh.subscribe("/hw/cam_dock", 1, dockCallback);
 
 	namedWindow("panel", CV_WINDOW_AUTOSIZE);
 	
-	createTrackbar("x", "panel", &x_init_red, 700); //Hue (0 - 255)
-	createTrackbar("y", "panel", &y_init_red, 700);
+	createTrackbar("x", "panel", &x_init, 700); //Hue (0 - 255)
+	createTrackbar("y", "panel", &y_init, 700);
 	createTrackbar("width", "panel", &width, 700); //Saturation (0 - 255)
 	createTrackbar("hight", "panel", &height, 700);
-	createTrackbar("noise", "panel", &Noise_red, 255);
+	createTrackbar("noise", "panel", &Noise, 255);
 	
 	while (ros::ok()) {
 		ros::spinOnce();
+		//imageProcessing(dock_image);
 	}
 }
 
 void imageProcessing(Mat input_image){
-	Mat imgOriginal, imgHSV, imgThresholded, imgErode, imgDilate, imgDebug;
-	
-	imgDebug = input_image.clone();
-	medianBlur(imgDebug, imgDebug, 5);
-	
-	erode(imgDebug, imgDebug, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) );
-	dilate( imgDebug, imgDebug, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
-	dilate(imgDebug, imgDebug, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
-	erode(imgDebug, imgDebug, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
-	
-	Rect region_of_interest = Rect(x_init_red, y_init_red, width, height);
+	/*
+	Rect region_of_interest = Rect(x_init, y_init, width, height);
 	Mat Original = input_image(region_of_interest);
 	
 	Size sz = Original.size();
 	int original_height = sz.height; 
 	int original_width	= sz.width;
-	
-	Size sx = input_image.size();
-	int input_height = sx.height;
-	int input_width = sx.width; 
-	
-	imgOriginal = Original.clone();
-	
-	medianBlur(imgOriginal, imgOriginal, 5);
-			
-	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) );
-	dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
 		
-	dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
-	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(Noise_red, Noise_red)) ); 
-	
-	line( input_image, Point( x_init_red, y_init_red ), Point( x_init_red+original_width, y_init_red), Scalar( 100, 100, 100 ), 2, 8 );
-	line( input_image, Point( x_init_red, y_init_red+original_height ), Point( x_init_red+original_width, y_init_red+original_height), Scalar( 100, 100, 100 ), 2, 8 );	
-	line( input_image, Point( x_init_red, y_init_red ), Point( x_init_red, y_init_red+original_height), Scalar( 100, 100, 100 ), 2, 8 );
-	line( input_image, Point( x_init_red+original_width, y_init_red ), Point( x_init_red+original_width, y_init_red+original_height), Scalar( 100, 100, 100 ), 2, 8 );
-	
-	//imshow("Threshold_Red", imgThresholded);
-	//imshow("Input_Red", input_image);
-	//imshow("Roi_Red", Original);
-	//imshow("All_Red", imgDebug);
+	line( input_image, Point( x_init, y_init ), Point( x_init+original_width, y_init), Scalar( 100, 100, 100 ), 2, 8 );
+	line( input_image, Point( x_init, y_init+original_height ), Point( x_init+original_width, y_init+original_height), Scalar( 100, 100, 100 ), 2, 8 );	
+	line( input_image, Point( x_init, y_init ), Point( x_init, y_init+original_height), Scalar( 100, 100, 100 ), 2, 8 );
+	line( input_image, Point( x_init+original_width, y_init ), Point( x_init+original_width, y_init+original_height), Scalar( 100, 100, 100 ), 2, 8 );
+	*/
+	imshow("Input_QR", input_image);
 }
